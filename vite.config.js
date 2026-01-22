50afeab28c2ac1eaa26ac2b55255aebe84407f1c
sightwebmaster <ahmed.benchaaben@etudiant-enit.utm.tn>
2026-01-22 23:25:51 +0100

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/MyPortfolio/', // <-- Add this line
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr'],
   server: {
